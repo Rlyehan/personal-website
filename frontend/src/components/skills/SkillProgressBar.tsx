777cd74bccb54c5aa6938589b116b9f2f6ff5785
Maximilian Huber <max_hub@gmx.net>
2023-04-26 15:49:00 +0200

    updated progress bar styling

--- frontend/src/components/skills/SkillProgressBar.tsx
+++ frontend/src/components/skills/SkillProgressBar.tsx
@@ -12,33 +12,37 @@ const SkillProgressBar: React.FC<SkillProgressBarProps> = ({
   isFirst,
 }) => {
   return (
-    <div className="my-2 mt-2">
+    <div className="my-4 w-3/4 mx-auto">
       <div className="flex justify-between items-center mb-1">
-        <span className="text-white w-1/5 mr-4">{skill}</span>
+        <span className="text-white font-semibold text-lg w-1/5 pr-4">
+          {skill}
+        </span>
         <div className="relative w-4/5">
           {isFirst && (
             <>
-              <div className="absolute top-0 left-1/4 text-s text-white -mt-8 transform -translate-x-1/2">
-                <span className="font-bold">Junior</span>
+              <div className="absolute top-0 left-1/4 text-base text-white -mt-12 transform -translate-x-1/2">
+                <span className="font-semibold">Junior</span>
               </div>
-              <div className="absolute top-0 left-1/2 text-s text-white -mt-8 transform -translate-x-1/2">
-                <span className="font-bold">Mid</span>
+              <div className="absolute top-0 left-1/2 text-base text-white -mt-12 transform -translate-x-1/2">
+                <span
+                  className="font-semibold"
+                  style={{ transform: 'translate(-50%, 0)' }}
+                >
+                  Mid
+                </span>
               </div>
-              <div className="absolute top-0 left-3/4 text-s text-white -mt-8 transform -translate-x-1/2">
-                <span className="font-bold">Senior</span>
+              <div className="absolute top-0 left-3/4 text-base text-white -mt-12 transform -translate-x-1/2">
+                <span className="font-semibold">Senior</span>
               </div>
             </>
           )}
-          <div className={`w-full h-5 rounded-lg bg-gray-800 mb-4 relative`}>
+          <div className={`w-full h-6 rounded-lg bg-gray-800 mb-4 relative`}>
             <div
-              className={`h-full rounded-lg ${
-                percentage >= 66
-                  ? 'bg-orange-800'
-                  : percentage >= 33
-                  ? 'bg-orange-500'
-                  : 'bg-orange-300'
-              } transition-all duration-500`}
-              style={{ width: `${percentage}%` }}
+              className={`h-full rounded-lg transition-all duration-500`}
+              style={{
+                width: `${percentage}%`,
+                backgroundImage: 'linear-gradient(to right, #FF5E5B, #FFAA00)',
+              }}
             ></div>
             <div className="absolute top-0 left-1/4 w-0.5 h-full bg-white opacity-40"></div>
             <div className="absolute top-0 left-1/2 w-0.5 h-full bg-white opacity-40"></div>
