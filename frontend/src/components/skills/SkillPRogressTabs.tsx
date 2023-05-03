8c4f1f114d3647e476014e87a518d3e8b768265b
Maximilian Huber <max_hub@gmx.net>
2023-05-03 10:38:07 +0200

    updated skill tabs

--- frontend/src/components/skills/SkillPRogressTabs.tsx
+++ frontend/src/components/skills/SkillPRogressTabs.tsx
@@ -2,22 +2,30 @@ import React, { useState } from 'react';
 import SkillProgressBar from './SkillProgressBar';
 
 const skillData = {
-  languages: [
-    { skill: 'Python', percentage: 80 },
-    { skill: 'JavaScript', percentage: 75 },
-    { skill: 'Go', percentage: 60 },
-    { skill: 'Rust', percentage: 50 },
+  dataEngineer: [
+    { skill: 'Placholder1', percentage: 80 },
+    { skill: 'Placholder2', percentage: 75 },
+    { skill: 'Placholder3', percentage: 60 },
+    { skill: 'Placholder4', percentage: 50 },
   ],
-  roles: [
-    { skill: 'Backend Developer', percentage: 80 },
-    { skill: 'Frontend Developer', percentage: 70 },
-    { skill: 'DevOps Engineer', percentage: 60 },
-    { skill: 'Data Engineer', percentage: 50 },
+  devopsEngineer: [
+    { skill: 'Placholder5', percentage: 80 },
+    { skill: 'Placholder6', percentage: 70 },
+    { skill: 'Placholder7', percentage: 60 },
+    { skill: 'Placholder8', percentage: 50 },
+  ],
+  backendDeveloper: [
+    { skill: 'Python', percentage: 80 },
+    { skill: 'Typrscript', percentage: 70 },
+    { skill: 'Go', percentage: 60 },
+    { skill: 'SQL Databases', percentage: 50 },
+    { skill: 'NoSQL Databases', percentage: 40 },
+    { skill: 'Rust', percentage: 30 },
   ],
 };
 
 const SkillProgressTabs: React.FC = () => {
-  const [activeTab, setActiveTab] = useState('languages');
+  const [activeTab, setActiveTab] = useState('dataEngineer');
 
   const renderSkillProgress = (
     skills: Array<{ skill: string; percentage: number }>,
@@ -34,26 +42,36 @@ const SkillProgressTabs: React.FC = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="flex justify-center mb-8">
+      <div className="flex justify-center mb-8 py-8">
         <button
           className={`px-4 py-2 font-semibold text-sm ${
-            activeTab === 'languages'
+            activeTab === 'dataEngineer'
               ? 'bg-darkGray text-darkOrange'
               : 'text-gray-300 hover:text-darkOrange hover:bg-gray-800'
           }`}
-          onClick={() => setActiveTab('languages')}
+          onClick={() => setActiveTab('dataEngineer')}
         >
-          Languages
+          Data Engineer
         </button>
         <button
           className={`px-4 py-2 font-semibold text-sm ${
-            activeTab === 'roles'
+            activeTab === 'backendDeveloper'
               ? 'bg-darkGray text-darkOrange'
               : 'text-gray-300 hover:text-darkOrange hover:bg-gray-800'
           }`}
-          onClick={() => setActiveTab('roles')}
+          onClick={() => setActiveTab('backendDeveloper')}
         >
-          Roles
+          Backend Developer
+        </button>
+        <button
+          className={`px-4 py-2 font-semibold text-sm ${
+            activeTab === 'devopsEngineer'
+              ? 'bg-darkGray text-darkOrange'
+              : 'text-gray-300 hover:text-darkOrange hover:bg-gray-800'
+          }`}
+          onClick={() => setActiveTab('devopsEngineer')}
+        >
+          DevOps Engineer
         </button>
       </div>
       {renderSkillProgress(skillData[activeTab as keyof typeof skillData])}
