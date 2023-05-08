0df67ef4a9da25eb3d5f853e0ab153b38f2fe843
Maximilian Huber <max_hub@gmx.net>
2023-05-08 18:43:00 +0200

    implemented search function

--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 
 interface ProjectCardProps {
-  id: number;
-  title: string;
+  repository_name: string;
   description: string;
-  githubUrl: string;
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({
-  id,
-  title,
+  repository_name,
   description,
-  githubUrl,
 }) => {
   return (
-    <div
-      className="bg-gray-800 rounded-lg p-4 cursor-pointer transition-colors duration-200 hover:bg-gray-700"
-      onClick={() => window.open(githubUrl, '_blank')}
-    >
-      <h2 className="text-2xl text-dark-orange font-semibold mb-2">{title}</h2>
+    <div className="bg-gray-800 rounded-lg p-4 cursor-pointer transition-colors duration-200 hover:bg-gray-700">
+      <h2 className="text-2xl text-dark-orange font-semibold mb-2">
+        {repository_name}
+      </h2>
       <p className="text-gray-200 text-sm mb-4">{description}</p>
     </div>
   );
--- frontend/src/pages/Projects.tsx
+++ frontend/src/pages/Projects.tsx
@@ -1,22 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ProjectCard from '../components/ProjectCard';
 
-const projectsData = [
-  {
-    id: 1,
-    title: 'Project 1',
-    description: 'This is a short description for Project 1',
-    imageUrl: 'https://via.placeholder.com/300',
-    githubUrl: 'https://github.com/username/project1',
-  },
-];
+interface ProjectData {
+  repository_name: string;
+  description: string;
+  tags: string[];
+}
 
-const Projects: React.FC = () => {
-  const [search, setSearch] = useState('');
+interface SearchResult {
+  repository_name: string;
+  description: string;
+  tags: string[];
+  score: number;
+}
 
-  //TODO: Implement search functionality
-  const performSearch = (query: string) => {
+const ProjectsPage: React.FC = () => {
+  const [search, setSearch] = useState<string>('');
+  const [projects, setProjects] = useState<ProjectData[]>([]);
+  const [displayedProjects, setDisplayedProjects] = useState<ProjectData[]>([]);
+
+  const fetchProjects = async () => {
+    const response = await axios.get('http://localhost/repositories', {
+      headers: { accept: 'application/json' },
+    });
+    setProjects(response.data);
+    setDisplayedProjects(response.data);
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  const performSearch = async (query: string) => {
     setSearch(query);
+    if (query) {
+      const response = await axios.post(`http://localhost/search`, {
+        text: query,
+      });
+      const searchResults: SearchResult[] = response.data;
+      console.log(searchResults);
+      const sortedResults = searchResults.sort((a, b) => b.score - a.score);
+      console.log(sortedResults);
+      const sortedProjects: ProjectData[] = sortedResults.map((result) => ({
+        repository_name: result.repository_name,
+        description: result.description,
+        tags: result.tags,
+      }));
+      console.log(sortedProjects);
+      setDisplayedProjects(sortedProjects);
+      setProjects(sortedProjects);
+    } else {
+      fetchProjects();
+    }
   };
 
   return (
@@ -29,12 +65,12 @@ const Projects: React.FC = () => {
         onChange={(e) => performSearch(e.target.value)}
       />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {projectsData.map((project) => (
-          <ProjectCard key={project.id} {...project} />
+        {displayedProjects.map((project, index) => (
+          <ProjectCard key={index} {...project} />
         ))}
       </div>
     </div>
   );
 };
 
-export default Projects;
+export default ProjectsPage;
